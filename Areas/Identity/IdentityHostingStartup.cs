c96eaed73c9db321334eedc9445ebdb5829e7b1d
dldygnl <53053198+itsjeffersonli@users.noreply.github.com>
2021-03-10 21:38:14 +0800

    Initial commit

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Negociantes.Areas.Identity.Data;
+using Negociantes.Data;
+
+[assembly: HostingStartup(typeof(Negociantes.Areas.Identity.IdentityHostingStartup))]
+namespace Negociantes.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<NegociantesContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("NegociantesContextConnection")));
+
+                services.AddDefaultIdentity<NegociantesUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<NegociantesContext>();
+            });
+        }
+    }
+}
---
+++ Data/UserApplication.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Negociantes.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace Negociantes.Data
+{
+    public class UserApplication :  IdentityDbContext<User>
+    {
+        public UserApplication(DbContextOptions<UserApplication> options) : base(options)
+        {
+        }
+    }
+}
